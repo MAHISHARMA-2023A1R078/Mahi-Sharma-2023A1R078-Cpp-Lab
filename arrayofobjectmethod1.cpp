3aa38eb90d96cbebc92acd3688c3ca827dc7f96e
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2025-02-26 18:22:06 +0530

    Add files via upload
    Array of object

---
+++ arrayofobjectmethod1.cpp
@@ -0,0 +1,23 @@
+/* Program to demonstrate working of Array of Object(Method 1) 25/02/25 */
+#include<iostream>
+using namespace std;
+class Program{  //class declaration
+    private: int Regno; float Marks; //Private Member of Class
+    public: Program(int R, float M){ //parametrized constructor of class //value ke saath memory intialize kewayega
+        Regno=R;
+        Marks = M;//local variable h
+    }
+    public: void Display(){ //function of class
+        cout<<"\n RegNumber= "<<Regno<<"\t"<<"Marks= "<<Marks;
+    }
+};
+int main(){
+    Program obj[3]= {{101,450}, {102,430}, {103,440}}; //Array of object with manual initialization
+     //jaise hi object create hhua too value bhi dedi..
+     //jis order mein constructor mein value pass hui ha ussi order mein display hogi beshak reverse loop chaloao
+    int i;
+    for(i=0;i<3;i++){
+        obj[i].Display(); //calling of Display Function with Array of Object
+    }
+    //obj[2].Display();  //value stored at this position too we can trace it sequentially...
+}
---
+++ studentmanagtsystem.cpp
@@ -0,0 +1,33 @@
+/* write a program 
+create a class with name student_Managenent_System
+store Student Data from RollNo 1 to 10
+Data includes(Name, RollNo, Percentage)
+Display All Data in Reverse Order using Array Of Object.
+*/
+
+#include<iostream>
+using namespace std;
+class Student_Management_System{
+    int R;
+    string N;
+    float P;
+    public: Student_Management_System(int RollNo, string Name, float Percentage){
+        //cout<<"store rollnos from 1 to 10\n";
+        R=RollNo;
+        N=Name;
+        P=Percentage;
+
+    }
+public: void display(){
+    cout<<"the RollNo = "<<R<<"\t"<<"the Name = "<<N<<"\t"<<"the Percentage = "<<P<<"\n";
+}
+};
+int main(){
+    Student_Management_System obj[10]={{1,"in",89},{2,"pop",45.09},{3,"tom",45.09},{4,"rock",45.09},{5,"jery",45.09},{6,"paid",45.09},
+    {7,"dack",45.09},{8,"pit",45.09},{9,"dock",45.09},{10,"nick",45.09}};
+    int i;
+    for (int i=0;i<10;i++){
+        obj[i].display();
+    }
+    return 0;
+}
