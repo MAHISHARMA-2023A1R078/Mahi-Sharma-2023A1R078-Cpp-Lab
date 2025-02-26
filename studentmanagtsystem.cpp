29a1d6ad5c7179c5d5b4e29eadeede9aed466e89
MAHISHARMA-2023A1R078 <146845167+MAHISHARMA-2023A1R078@users.noreply.github.com>
2025-02-26 20:31:07 +0530

    Update studentmanagtsystem.cpp

--- studentmanagtsystem.cpp
+++ studentmanagtsystem.cpp
@@ -26,7 +26,7 @@ int main(){
     Student_Management_System obj[10]={{1,"in",89},{2,"pop",45.09},{3,"tom",45.09},{4,"rock",45.09},{5,"jery",45.09},{6,"paid",45.09},
     {7,"dack",45.09},{8,"pit",45.09},{9,"dock",45.09},{10,"nick",45.09}};
     int i;
-    for (int i=0;i<10;i++){
+    for (int i=9;i>=0;i--){
         obj[i].display();
     }
     return 0;
